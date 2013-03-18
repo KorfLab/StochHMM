a3158a3e641a2fc47e8a071a360a814953c2b329
Paul Lott <plott@ucdavis.edu>
2013-03-18 13:24:04 -0700

    Update version tag

--- src/StochHMM_usage.h
+++ src/StochHMM_usage.h
@@ -10,7 +10,7 @@
 #define StochHMM_StochHMM_usage_h
 
 const char usage[]  = "\n\
-StochHMM - Stochastic Hidden Markov Model Framework (version 0.27) Date: March 7, 2013\n\
+StochHMM - Stochastic Hidden Markov Model Framework (version 0.28) Date: March 7, 2013\n\
 \n\
 Usage: StochHMM -model <model_file>  -seq <seq_file> [options]\n\
 \n\
