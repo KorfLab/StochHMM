b09dec528e3769899c0213170bb9e7f30f3f87ca
Paul Lott <plott@ucdavis.edu>
2015-11-16 08:10:02 -0800

    Added multi-independent emission dice model to stochhmm

--- xcode/StringDice/main.cpp
+++ xcode/StringDice/main.cpp
@@ -2,7 +2,14 @@
 //  main.cpp
 //  StringDice
 //
-//  Created by Paul Lott on 11/9/15.
+//  Created by Paul Lott on 11/16/15.
+//  Copyright © 2015 Korf Lab, Genome Center, UC Davis, Davis, CA. All rights reserved.
+//
+
+//
+//  main.cpp
+//  StringDice
+//
 //  Copyright © 2015 Korf Lab, Genome Center, UC Davis, Davis, CA. All rights reserved.
 //
 
@@ -12,21 +19,29 @@
 int main(int argc, const char * argv[]) {
 	
 	//Import HMM
-	std::string model_file = "Dice.hmm";
+	std::string model_file = "Dice_multi.hmm";
 	StochHMM::model hmm;
 	hmm.import(model_file);
-	hmm.random_walk();
+	
+	hmm.print();
 	
 	//Get Track from Model
-	StochHMM::track* tr = hmm.getTrack(0);
+	StochHMM::track* dice_tr = hmm.getTrack(0);
+	StochHMM::track* color_tr = hmm.getTrack(1);
+	
 	
 	//Convert String to sequence
-	std::string sq("123456123456");
-	StochHMM::sequence* seq = new StochHMM::sequence(sq, tr);
+	std::string sq("123456664");
+	std::string col("RGBBBRRRB");
+	StochHMM::sequence* seq = new StochHMM::sequence(sq, dice_tr);
+	StochHMM::sequence* col_seq = new StochHMM::sequence(col, color_tr);
 	
 	//Add sequence to sequences
 	StochHMM::sequences seqs;
-	seqs.addSeq(seq, tr);
+	seqs.addSeq(seq, dice_tr);
+	seqs.addSeq(col_seq, color_tr);
+	
+	seqs.print();
 	
 	//Process Model
 	StochHMM::trellis trell(&hmm,&seqs);
@@ -37,6 +52,7 @@ int main(int argc, const char * argv[]) {
 	std::cout << "Score: " << path->getScore() << std::endl;
 	path->print_label();
 	
-
-    return 0;
+	
+	return 0;
 }
+
